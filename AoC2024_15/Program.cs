02b08f7bfaca99fed93ce7b5680e19a02c786cbc
Scott Bakker <Scott@MeowPC>
2024-12-17 17:04:54 -0500

    Day 15 part 1

---
+++ AoC2024_15/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace AoC2024_15;
+
+internal class Program
+{
+    private const string inputFile = "C:\\Users\\Scott\\source\\repos\\AdventOfCode2024\\Input\\Day_15.txt";
+    private const char robotChar = '@';
+    private const char boxChar = 'O';
+    private const char wallChar = '#';
+    private const char floorChar = '.';
+
+    static void Main()
+    {
+        Puzzle1();
+        Console.WriteLine();
+        Puzzle2();
+        Console.WriteLine();
+        Console.Write("Press enter to continue...");
+        Console.ReadLine();
+    }
+
+    static void Puzzle1()
+    {
+        long answer = 0;
+        var lines = File.ReadAllLines(inputFile);
+        var height = 0;
+        var width = lines[0].Length;
+        while (lines[height] != "")
+        {
+            height++;
+        }
+        var grid = new char[height, width];
+        Robot robot = new();
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                switch (lines[y][x])
+                {
+                    case wallChar:
+                        grid[y, x] = wallChar;
+                        break;
+                    case robotChar:
+                        robot.Y = y;
+                        robot.X = x;
+                        grid[y, x] = robotChar;
+                        break;
+                    case boxChar:
+                        grid[y, x] = boxChar;
+                        break;
+                    default:
+                        grid[y, x] = floorChar;
+                        break;
+                }
+            }
+        }
+        foreach (string s in lines[height..])
+        {
+            foreach (char c in s)
+            {
+                int moveY = 0;
+                int moveX = 0;
+                switch (c)
+                {
+                    case '^':
+                        moveY = -1;
+                        break;
+                    case '>':
+                        moveX = 1;
+                        break;
+                    case 'v':
+                        moveY = 1;
+                        break;
+                    case '<':
+                        moveX = -1;
+                        break;
+                }
+                if (moveY == 0 && moveX == 0) continue;
+                if (grid[robot.Y + moveY, robot.X + moveX] == wallChar)
+                {
+                    continue; // hit wall, don't move
+                }
+                if (grid[robot.Y + moveY, robot.X + moveX] == floorChar)
+                {
+                    grid[robot.Y, robot.X] = floorChar;
+                    robot.Y += moveY;
+                    robot.X += moveX;
+                    grid[robot.Y, robot.X] = robotChar;
+                    continue;
+                }
+                // hit a box
+                int testY = robot.Y + moveY;
+                int testX = robot.X + moveX;
+                while (grid[testY, testX] == boxChar)
+                {
+                    testY += moveY;
+                    testX += moveX;
+                }
+                if (grid[testY, testX] == wallChar)
+                {
+                    // hit a wall, can't move boxes
+                    continue;
+                }
+                // move box to end (don't have to move whole line!)
+                grid[testY, testX] = boxChar;
+                grid[robot.Y, robot.X] = floorChar;
+                robot.Y += moveY;
+                robot.X += moveX;
+                grid[robot.Y, robot.X] = robotChar;
+            }
+        }
+        // show result
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[y, x] == boxChar)
+                {
+                    answer += (y * 100) + x;
+                }
+                Console.Write(grid[y, x]);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Day 15 Puzzle 1 Answer = {answer}");
+    }
+
+    static void Puzzle2()
+    {
+        long answer = 0;
+        var lines = File.ReadAllLines(inputFile);
+        // ...
+        Console.WriteLine($"Day 15 Puzzle 2 Answer = {answer}");
+    }
+}
---
+++ AoC2024_15/Robot.cs
@@ -0,0 +1,7 @@
+﻿namespace AoC2024_15;
+
+public class Robot
+{
+    public int Y { get; set; } = 0;
+    public int X { get; set; } = 0;
+}
