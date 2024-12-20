ef33e97755f95599932d2557d8ed1c6abe526502
Scott Bakker <Scott@MeowPC>
2024-12-20 17:59:05 -0500

    Day 16 Part 1

---
+++ AoC2024_16/Junction.cs
@@ -0,0 +1,16 @@
+﻿namespace AoC2024_16;
+
+public class Junction : Point
+{
+    public int MinScoreToHere { get; set; } = int.MaxValue;
+
+    public Junction()
+    {
+    }
+
+    public Junction(int y, int x)
+    {
+        Y = y;
+        X = x;
+    }
+}
--- AoC2024_16/Program.cs
+++ AoC2024_16/Program.cs
@@ -1,15 +1,26 @@
-﻿namespace AoC2024_16;
+﻿
+namespace AoC2024_16;
 
 internal class Program
 {
-    private const string inputFile = "C:\\Users\\Scott\\source\\repos\\AdventOfCode2024\\Input\\Day_16_Test.txt";
+    private const string inputFile = "C:\\Users\\Scott\\source\\repos\\AdventOfCode2024\\Input\\Day_16.txt";
     private const char wallChar = '#';
     private const char floorChar = '.';
     private const char startChar = 'S';
     private const char endChar = 'E';
 
+    private const bool showPath = false;
+
     private static int height = 0;
     private static int width = 0;
+    private static char[,] grid = new char[0, 0];
+    private static int[,] score = new int[0, 0];
+    private static Point startPos = new();
+    private static Point endPos = new();
+
+    // 0 = north, 1 = east, 2 = south, 3 = west
+    private static readonly int[] NextMoveY = [-1, 0, 1, 0];
+    private static readonly int[] NextMoveX = [0, 1, 0, -1];
 
     static void Main()
     {
@@ -27,23 +38,88 @@ internal class Program
         var lines = File.ReadAllLines(inputFile);
         height = lines.Length;
         width = lines[0].Length;
-        Point startPos;
-        Point endPos;
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
+        grid = new char[height, width];
+        score = new int[height, width];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                score[y, x] = int.MaxValue;
                 if (lines[y][x] == startChar)
                 {
                     startPos = new Point(y, x);
+                    grid[y, x] = floorChar;
                 }
                 else if (lines[y][x] == endChar)
                 {
                     endPos = new Point(y, x);
+                    grid[y, x] = floorChar;
+                }
+                else
+                {
+                    grid[y, x] = lines[y][x];
                 }
             }
         }
-
-        // ...
+        // facing east at start
+        score[startPos.Y, startPos.X] = 0;
+        CheckPath(startPos.Y - 1, startPos.X, 0, 1001); // left 90-degree turn north
+        CheckPath(startPos.Y, startPos.X + 1, 1, 1); // one step forward
+        CheckPath(startPos.Y + 1, startPos.X, 2, 1001); // right 90-degree turn south
+        if (showPath)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (grid[y, x] == wallChar)
+                    {
+                        Console.Write("      #");
+                    }
+                    else if (score[y, x] == int.MaxValue)
+                    {
+                        Console.Write("      .");
+                    }
+                    else
+                    {
+                        Console.Write($"{score[y, x],7}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        // get answer
+        answer = score[endPos.Y, endPos.X];
         Console.WriteLine($"Day 16 Puzzle 1 Answer = {answer}");
+    }
+
+    private static void CheckPath(int y, int x, int facing, int scoreToHere)
+    {
+        if (grid[y, x] == wallChar)
+        {
+            return;
+        }
+        if (score[y, x] <= scoreToHere)
+        {
+            return;
+        }
+        score[y, x] = scoreToHere;
+        if (endPos.Y == y && endPos.X == x)
+        {
+            return; // at end, don't search any more
+        }
+        CheckPath(y + NextMoveY[(facing + 3) % 4],
+                  x + NextMoveX[(facing + 3) % 4],
+                  (facing + 3) % 4,
+                  scoreToHere + 1001);
+        CheckPath(y + NextMoveY[facing],
+                  x + NextMoveX[facing],
+                  facing,
+                  scoreToHere + 1);
+        CheckPath(y + NextMoveY[(facing + 1) % 4],
+                  x + NextMoveX[(facing + 1) % 4],
+                  (facing + 1) % 4,
+                  scoreToHere + 1001);
     }
 
     static void Puzzle2()
