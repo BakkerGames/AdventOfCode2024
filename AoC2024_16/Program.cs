d64a28b1e01e6623de7b4990ce2346915a1ca365
Scott Bakker <Scott@MeowPC>
2024-12-26 17:27:50 -0500

    Day 16 part 2 not working

--- AoC2024_16/Program.cs
+++ AoC2024_16/Program.cs
@@ -1,16 +1,14 @@
-﻿
-
-namespace AoC2024_16;
+﻿namespace AoC2024_16;
 
 internal class Program
 {
-    private const string inputFile = "C:\\Users\\Scott\\source\\repos\\AdventOfCode2024\\Input\\Day_16_Test_1.txt";
+    private const string inputFile = "C:\\Users\\Scott\\source\\repos\\AdventOfCode2024\\Input\\Day_16.txt";
     private const char wallChar = '#';
     private const char floorChar = '.';
     private const char startChar = 'S';
     private const char endChar = 'E';
 
-    private const bool showPath = true;
+    private const bool showPath = false;
 
     private static int height = 0;
     private static int width = 0;
@@ -19,6 +17,8 @@ internal class Program
     private static bool[,] bestSeat = new bool[0, 0];
     private static Point startPos = new();
     private static Point endPos = new();
+    private static int bestPathScore;
+    private static List<Point> bestPathPoints = [];
 
     // 0 = north, 1 = east, 2 = south, 3 = west
     private static readonly int[] NextMoveY = [-1, 0, 1, 0];
@@ -127,48 +127,131 @@ internal class Program
     static void Puzzle2()
     {
         long answer = 0;
-        // use the grid[,] and score[,] arrays from Puzzle 1 to solve Puzzle 2
+        var lines = File.ReadAllLines(inputFile);
+        height = lines.Length;
+        width = lines[0].Length;
+        grid = new char[height, width];
+        score = new int[height, width];
         bestSeat = new bool[height, width];
-        CheckSeats(endPos.Y, endPos.X);
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                if (bestSeat[y, x]) answer++;
-                if (showPath)
+                score[y, x] = int.MaxValue;
+                if (lines[y][x] == startChar)
                 {
-                    if (bestSeat[y, x])
-                        Console.Write('O');
-                    else
-                        Console.Write(grid[y, x]);
+                    startPos = new Point(y, x);
+                    grid[y, x] = floorChar;
+                }
+                else if (lines[y][x] == endChar)
+                {
+                    endPos = new Point(y, x);
+                    grid[y, x] = floorChar;
+                }
+                else
+                {
+                    grid[y, x] = lines[y][x];
                 }
             }
-            if (showPath)
+        }
+        bestPathScore = int.MaxValue;
+        // facing east at start
+        score[startPos.Y, startPos.X] = 0;
+        CheckPath(startPos.Y - 1, startPos.X, 0, 1001); // left 90-degree turn north
+        CheckPath(startPos.Y, startPos.X + 1, 1, 1); // one step forward
+        CheckPath(startPos.Y + 1, startPos.X, 2, 1001); // right 90-degree turn south
+        bestPathScore = score[endPos.Y, endPos.X];
+        List<Point> points = [];
+        points.Add(new Point(startPos.Y, startPos.X));
+        CheckPath2(startPos.Y - 1, startPos.X, 0, 1001, points); // left 90-degree turn north
+        CheckPath2(startPos.Y, startPos.X + 1, 1, 1, points); // one step forward
+        CheckPath2(startPos.Y + 1, startPos.X, 2, 1001, points); // right 90-degree turn south
+        foreach (Point p in bestPathPoints)
+        {
+            bestSeat[p.Y, p.X] = true;
+        }
+        if (showPath)
+        {
+            for (int y = 0; y < height; y++)
             {
+                for (int x = 0; x < width; x++)
+                {
+                    if (grid[y, x] == wallChar)
+                    {
+                        Console.Write("#");
+                    }
+                    else if (bestSeat[y, x])
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
                 Console.WriteLine();
+            }
+        }
+        // get answer
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (bestSeat[y, x])
+                {
+                    answer++;
+                }
             }
         }
         Console.WriteLine($"Day 16 Puzzle 2 Answer = {answer}");
     }
 
-    private static void CheckSeats(int y, int x)
+    private static void CheckPath2(int y, int x, int facing, int scoreToHere, List<Point> points)
     {
-        bestSeat[y, x] = true;
-        if (grid[y - 1, x] == floorChar && score[y - 1, x] < score[y, x])
+        if (grid[y, x] == wallChar)
         {
-            CheckSeats(y - 1, x);
+            return;
         }
-        if (grid[y, x + 1] == floorChar && score[y, x + 1] < score[y, x])
+        if (scoreToHere > bestPathScore)
         {
-            CheckSeats(y, x + 1);
+            // this path will never work
+            return;
         }
-        if (grid[y + 1, x] == floorChar && score[y + 1, x] < score[y, x])
+        Point p = new(y, x);
+        if (points.Contains(p))
         {
-            CheckSeats(y + 1, x);
+            // looped back onto path
+            return;
         }
-        if (grid[y, x - 1] == floorChar && score[y, x - 1] < score[y, x])
+        List<Point> newPoints = [];
+        newPoints.AddRange(points);
+        newPoints.Add(p);
+        if (endPos.Y == y && endPos.X == x)
         {
-            CheckSeats(y, x - 1);
+            foreach (Point p1 in newPoints)
+            {
+                if (!bestPathPoints.Contains(p1))
+                {
+                    bestPathPoints.Add(p1);
+                }
+            }
+            Console.WriteLine($"{bestPathScore} - {bestPathPoints.Count}");
+            return; // at end, don't search any more
         }
+        CheckPath2(y + NextMoveY[(facing + 3) % 4],
+                  x + NextMoveX[(facing + 3) % 4],
+                  (facing + 3) % 4,
+                  scoreToHere + 1001,
+                  newPoints);
+        CheckPath2(y + NextMoveY[facing],
+                  x + NextMoveX[facing],
+                  facing,
+                  scoreToHere + 1,
+                  newPoints);
+        CheckPath2(y + NextMoveY[(facing + 1) % 4],
+                  x + NextMoveX[(facing + 1) % 4],
+                  (facing + 1) % 4,
+                  scoreToHere + 1001,
+                  newPoints);
     }
 }
